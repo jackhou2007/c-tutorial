1627b9d8c09f022bad537d57fd3219fef4d7d973
jackhou2007 <jackhou2007@gmail.com>
2015-02-03 19:17:11 +0800

    add new file

---
+++ string/print_without_semicolon.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	if (printf("Hello World\n"))
+	{
+
+	}
+
+	while (!printf("Hello World\n"))
+	{
+
+	}
+
+	switch (printf("Hello World\n"))
+	{
+	
+	}
+
+	return 0;
+}
